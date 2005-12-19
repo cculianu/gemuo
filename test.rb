27c1cac3eae66701cf49511c2949dfa69be3a2f1
Max Kellermann <max@duempel.org>
2005-12-19 22:18:08 +0000

    more test code
    git-svn-id: svn://svn.berlios.de/gemuo/trunk@19 4ec853c9-4508-0410-a67d-f814b9f2780e

--- test.rb
+++ test.rb
@@ -2,10 +2,26 @@ $:.unshift(File.dirname($0) + '/glue')
 $:.unshift(File.dirname($0))
 
 require 'uo/client'
+require 'uo/engines'
 
 raise "syntax: test.rb host port username password" unless ARGV.length == 4
 
-client = UO::Client.new(ARGV[0], ARGV[1], nil,
-                        ARGV[2], ARGV[3])
+$client = UO::Client.new(ARGV[0], ARGV[1], nil,
+                         ARGV[2], ARGV[3])
 
-client.run
+class TestTimer < UO::TimerEvent
+    def tick
+        puts "tick!\n"
+    end
+end
+
+class Ingame
+    def on_ingame
+        UO::Engines::SimpleWalk.new($client, UO::Position.new(1420, 1675))
+        $client.timer << TestTimer.new(10)
+    end
+end
+
+$client.signal_connect(Ingame.new)
+
+$client.run
