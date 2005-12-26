e73b0c6f19e4547d561e22f4300a47a8b4891cf1
Max Kellermann <max@duempel.org>
2005-12-26 13:59:27 +0000

    use Main engine
    git-svn-id: svn://svn.berlios.de/gemuo/trunk@106 4ec853c9-4508-0410-a67d-f814b9f2780e

--- test.rb
+++ test.rb
@@ -2,6 +2,7 @@ $:.unshift(File.dirname($0) + '/glue')
 $:.unshift(File.dirname($0))
 
 require 'uo/client'
+require 'uo/engines/main'
 require 'uo/engines/collect'
 require 'uo/engines/debug'
 require 'uo/engines/stack'
@@ -10,38 +11,10 @@ require 'uo/engines/stats'
 
 raise "syntax: test.rb host port username password charname str dex int" unless ARGV.length == 8
 
-$client = UO::Client.new(ARGV[0], ARGV[1], nil,
-                         ARGV[2], ARGV[3], ARGV[4])
+client = UO::Client.new(ARGV[0], ARGV[1], nil,
+                        ARGV[2], ARGV[3], ARGV[4])
 
-$stats_goal = [ARGV[5].to_i, ARGV[6].to_i, ARGV[7].to_i]
-
-class Ingame
-    def on_ingame
-        #e = UO::Engines::CollectItems.new($client, 0xdf9) # collect wool
-        #e = UO::Engines::CollectItems.new($client, 0x175d) # collect oil cloth
-
-        skills = [ UO::SKILL_ANATOMY,
-                   UO::SKILL_ITEM_ID,
-                   UO::SKILL_ARMS_LORE,
-                   #UO::SKILL_DETECT_HIDDEN,
-                   UO::SKILL_EVAL_INT,
-                   UO::SKILL_HIDING,
-                   UO::SKILL_MUSICIANSHIP,
-                   UO::SKILL_PEACEMAKING,
-                   #UO::SKILL_SPIRIT_SPEAK,
-                 ]
-        e = UO::Engines::EasySkills.new($client, skills)
-        # e = UO::Engines::StackItems.new($client, 0x1f4c) # recall scrolls
-
-        e.start
-
-        e = UO::Engines::StatLock.new($client, $stats_goal)
-        e.start
-
-        # UO::Engines::SimpleWalk.new($client, UO::Position.new(1410, 1735))
-        # $client.timer << TestTimer.new(10)
-    end
-end
+stats_goal = [ARGV[5].to_i, ARGV[6].to_i, ARGV[7].to_i]
 
 pos_bridge_left = UO::Position.new(1370, 1749)
 pos_bridge_right = UO::Position.new(1401, 1749)
@@ -54,10 +27,25 @@ cottonfeld1 = UO::Position.new(1237, 1618)
 pearfeld1 = UO::Position.new(1165, 1594)
 cotton_eingang = UO::Position.new(4569, 1480)
 
+engines = []
 
-$client.signal_connect(Ingame.new)
+skills = [ UO::SKILL_ANATOMY,
+           UO::SKILL_ITEM_ID,
+           UO::SKILL_ARMS_LORE,
+           #UO::SKILL_DETECT_HIDDEN,
+           UO::SKILL_EVAL_INT,
+           UO::SKILL_HIDING,
+           #UO::SKILL_MUSICIANSHIP,
+           #UO::SKILL_PEACEMAKING,
+           #UO::SKILL_SPIRIT_SPEAK,
+         ]
+engines << UO::Engines::EasySkills.new(client, skills)
+engines << UO::Engines::StatLock.new(client, stats_goal)
+
+UO::Engines::Main.new(client, engines).start
+
 # $client.signal_connect(UO::Engines::EntityDump.new)
 # $client.signal_connect(UO::Engines::WalkDump.new)
 # $client.signal_connect(StatSkillJojo.new(UO::SKILL_ARMSLORE, UO::SKILL_ITEMID))
 
-$client.run
+client.run
