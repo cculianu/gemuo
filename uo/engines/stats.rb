f6c532a2655a91995dcf568379b84f39b671e337
Max Kellermann <max@duempel.org>
2005-12-26 13:48:18 +0000

    added stat engine
    git-svn-id: svn://svn.berlios.de/gemuo/trunk@102 4ec853c9-4508-0410-a67d-f814b9f2780e

---
+++ uo/engines/stats.rb
@@ -0,0 +1,81 @@
+#
+#  libuoclient-ruby
+#  $Id$
+#
+#  (c) 2005 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+module UO::Engines
+    class StatLock < UO::TimerEvent
+        def initialize(client, goal)
+            @client = client
+            @goal = goal
+        end
+
+        def start
+            @client.signal_connect(self)
+            check_stats
+        end
+        def stop
+            @client.signal_disconnect(self)
+        end
+
+        def set_lock(stat, lock)
+            if @locks[stat] != lock
+                puts "Setting stat lock #{UO::STAT_NAMES[stat]} to #{UO::LOCK_NAMES[lock]}\n"
+                @client << UO::Packet::StatLock.new(stat, lock)
+            end
+        end
+
+        def check_stats
+            player = @client.world.player
+            return unless player
+
+            cap = player.stat_cap || 225
+            stats = player.stats || [0, 0, 0]
+            @locks = player.stat_locks || [nil, nil, nil]
+            sum = stats[0] + stats[1] + stats[2]
+
+            unless @locks
+                puts "no stat lock info available\n"
+                return
+            end
+
+            if sum < cap
+                (0..2).each do
+                    |stat|
+                    set_lock(stat, UO::LOCK_UP)
+                end
+                return
+            end
+
+            (0..2).each do
+                |stat|
+                if stats[stat] > @goal[stat]
+                    set_lock(stat, UO::LOCK_DOWN)
+                elsif stats[stat] < @goal[stat]
+                    set_lock(stat, UO::LOCK_UP)
+                else
+                    set_lock(stat, UO::LOCK_LOCKED)
+                end
+            end
+        end
+
+        def on_mobile_status(mobile)
+            check_stats if mobile == @client.world.player
+        end
+    end
+end
