e1a4efb2addeb87133882fdd11e743eabe4bdd9c
Max Kellermann <max@duempel.org>
2005-12-26 01:52:43 +0000

    added melee trainer
    git-svn-id: svn://svn.berlios.de/gemuo/trunk@98 4ec853c9-4508-0410-a67d-f814b9f2780e

---
+++ melee.rb
@@ -0,0 +1,178 @@
+#
+#  libuoclient-ruby
+#  $Id$
+#
+#  (c) 2005 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+$:.unshift(File.dirname($0) + '/glue')
+$:.unshift(File.dirname($0))
+
+require 'uo/client'
+require 'uo/engines/main'
+require 'uo/engines/blocking'
+
+raise "syntax: test.rb host port username password charname target_serial" unless ARGV.length == 6
+
+class Melee < UO::Engines::Blocking
+    PRIMARY = [ UO::SKILL_SWORDS, UO::SKILL_MACING, UO::SKILL_FENCING, UO::SKILL_WRESTLING ]
+    SECONDARY = [ UO::SKILL_TACTICS, UO::SKILL_ANATOMY, UO::SKILL_LUMBERJACKING,
+                  UO::SKILL_HEALING, UO::SKILL_PARRY ]
+
+    def initialize(client, target_serial, passive)
+        super(client)
+        @target_serial = target_serial
+        @passive = passive
+    end
+
+    def arm_for_skill(skill_id)
+        # XXX
+    end
+
+    def run
+        loop do
+            # check skill values
+            primary = []
+            done = []
+            secondary = []
+
+            skills.each_value do
+                |skill|
+                next if skill.lock != UO::SKILL_LOCK_UP
+                if PRIMARY.include?(skill.id)
+                    if skill.base < skill.cap && skill.lock == UO::SKILL_LOCK_UP
+                        primary << skill.id
+                    else
+                        done << skill.id
+                    end
+                elsif SECONDARY.include?(skill.id)
+                    if skill.base < skill.cap && skill.lock == UO::SKILL_LOCK_UP
+                        secondary << skill.id 
+                    end
+                end
+            end
+
+            unless @passive
+                if primary.empty? && secondary.empty?
+                    return true
+                end
+
+                if skill_free == 0
+                    raise "No skillpoints free"
+                end
+
+                if primary.empty? && done.empty?
+                    raise "No primary skills available"
+                end
+            end
+
+            # determine next training skill
+            primary.sort!
+            done.sort!
+
+            unless @passive
+                line = "Melee skills:"
+                [primary, secondary].flatten.each do
+                    |skill_id|
+                    skill = skills[skill_id]
+                    line << "  #{skill.name}=#{skill.base}"
+                end
+                line << "\n"
+                puts line
+            end
+
+            current = primary.empty? ? done.last : primary.last
+
+            arm_for_skill(current)
+
+            # check target health
+            target = @client.world.entity(@target_serial)
+            raise "Target not found" unless target
+
+            target_hits = target.hits
+            puts "target_hits=#{target_hits}\n"
+            raise "No health info for target" unless target_hits
+
+            if target_hits.value <= (target_hits.max * 2) / 3
+                # disable war mode
+                @client << UO::Packet::WarMode.new(false)
+
+                # heal target
+                if secondary.include?(UO::SKILL_HEALING) ||
+                        (skills[UO::SKILL_HEALING] && skills[UO::SKILL_HEALING].value >= 300)
+                    # got healing skill
+                    puts "Healing target #{target}\n"
+
+                    bandage = item_in_backpack(0xe21)
+                    puts "bandage=#{bandage}\n"
+                    if bandage
+                        use_and_target(bandage, target)
+                    else
+                        puts "No bandage in backpack\n"
+                    end
+                else
+                    # no healing skill available, wait for target to heal itself
+                end
+
+                sleep(13)
+                puts "after heal sleep\n"
+
+                next
+            end
+
+            # check my health
+            player = @client.world.player
+            raise "Player not found" unless player
+
+            player_hits = player.hits
+            puts "player_hits=#{player_hits}\n"
+            raise "No health info for player" unless target_hits
+
+            if player_hits.value <= (player_hits.max * 2) / 3
+                # disable war mode
+                @client << UO::Packet::WarMode.new(false)
+
+                # heal myself
+                if secondary.include?(UO::SKILL_HEALING) ||
+                        (skills[UO::SKILL_HEALING] && skills[UO::SKILL_HEALING].value >= 300)
+                    # got healing skill
+                    puts "Healing myself\n"
+                else
+                    # no healing skill available, wait for target to heal us
+                    sleep(13)
+                end
+
+                next
+            end
+
+            # attack
+            @client << UO::Packet::WarMode.new(true)
+            @client << UO::Packet::Attack.new(@target_serial)
+            
+            # idle for a moment
+            sleep(2)
+        end
+    end
+end
+
+client = UO::Client.new(ARGV[0], ARGV[1], nil,
+                        ARGV[2], ARGV[3], ARGV[4])
+
+target_serial = ARGV[5].hex
+
+UO::Engines::Main.new(client, Melee.new(client, target_serial, false)).start
+
+client.run
