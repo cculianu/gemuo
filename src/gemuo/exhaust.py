23104b8a019f3b83e9a5567d8fddb2564c433fed
Max Kellermann <max@duempel.org>
2010-03-07 16:31:26 +0100

    added resource finder and exhaustion database

---
+++ src/gemuo/exhaust.py
@@ -0,0 +1,48 @@
+#
+#  GemUO
+#
+#  (c) 2005-2010 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+
+import os
+import anydbm
+
+class ExhaustDatabase:
+    """A file-based database of blocks whose resources are exhausted."""
+
+    def __init__(self, path):
+        self._db = anydbm.open(path, 'c')
+
+    def _key(self, x, y):
+        return '%d,%d' % (x, y)
+
+    def is_exhausted(self, x, y):
+        key = self._key(x, y)
+        try:
+            t = self._db[key]
+        except KeyError:
+            return False
+        now = os.times()[4]
+        if now < float(t):
+            return True
+        else:
+            # record is expired; delete it
+            print "exhausted expired:", x, y
+            del self._db[key]
+            return False
+
+    def set_exhausted(self, x, y):
+        print "set exhausted:", x, y
+        key = self._key(x, y)
+        now = os.times()[4]
+        # this block is exhausted for 30 minutes
+        self._db[key] = str(now + 30 * 60)
---
+++ src/gemuo/resource.py
@@ -0,0 +1,50 @@
+#
+#  GemUO
+#
+#  (c) 2005-2010 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+
+VECTORS = (
+    (0, -1),
+    (-1, 0),
+    (0, 1),
+    (1, 0),
+)
+
+class Spiral:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.length = 1
+        self.remaining = 0
+        self.direction = 0
+
+    def step(self):
+        if self.remaining == 0:
+            self.length += 1
+            self.direction = (self.direction + 1) % len(VECTORS)
+            self.vector = VECTORS[self.direction]
+            self.remaining = self.length / 2
+        self.x += self.vector[0]
+        self.y += self.vector[1]
+        self.remaining -= 1
+
+def find_resource(map, position, ids, exhaust_db):
+    spiral = Spiral(position.x / 8, position.y / 8)
+    while True:
+        if not exhaust_db.is_exhausted(spiral.x, spiral.y):
+            block = map.statics.load_block(spiral.x, spiral.y)
+            if block is not None:
+                for item_id, x, y, z, hue in block:
+                    if ((item_id & 0x3fff) | 0x4000) in ids:
+                        return item_id, spiral.x * 8 + x, spiral.y * 8 + y, z
+        spiral.step()
