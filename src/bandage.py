2d2890e925c4729a1a62bad418b0eb67af920c52
Max Kellermann <max@duempel.org>
2010-03-14 15:08:45 +0100

    bandage: open backpack

--- src/bandage.py
+++ src/bandage.py
@@ -4,8 +4,10 @@ from uo.skills import *
 from gemuo.simple import SimpleClient
 from gemuo.engine.messages import PrintMessages
 from gemuo.engine.bandage import CutCloth
+from gemuo.engine.items import OpenContainer
 
 client = SimpleClient()
 PrintMessages(client)
+client.until(OpenContainer(client, client.world.backpack()).finished)
 cc = CutCloth(client)
 client.until(cc.finished)
