fc863818178af517d1540cc4ff41d248e0dcc0af
Kai Sassmannshausen <kai@sassie.org>
2010-03-19 08:12:20 +0100

    implemented spells and cast support
    [mk: ported Ruby code over to Python]

--- src/uo/packets.py
+++ src/uo/packets.py
@@ -413,15 +413,18 @@ def TextCommand(type, command):
     p.cstring(command)
     return p.finish()
 
+def UseSkill(skill):
+    return TextCommand(0x24, str(skill))
+
+def Cast(spell):
+    return TextCommand(0x56, str(spell))
+
 def EquipRequest(item_serial, layer, target_serial):
     p = PacketWriter(0x13)
     p.uint(item_serial)
     p.byte(layer)
     p.uint(target_serial)
     return p.finish()
-
-def UseSkill(skill):
-    return TextCommand(0x24, str(skill))
 
 def MobileQuery(type, serial):
     p = PacketWriter(0x34)
