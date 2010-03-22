c46db974c9826f22fbcd0c8c1c1d280a78ab99f0
Max Kellermann <max@duempel.org>
2010-03-22 22:17:03 +0100

    CutCloth: separate CutCloth / CutAllCloth

--- src/bandage.py
+++ src/bandage.py
@@ -3,11 +3,12 @@
 from uo.skills import *
 from gemuo.simple import SimpleClient
 from gemuo.engine.messages import PrintMessages
-from gemuo.engine.bandage import CutCloth
+from gemuo.engine.bandage import CutAllCloth
 from gemuo.engine.items import OpenContainer
 
 client = SimpleClient()
 PrintMessages(client)
 client.until(OpenContainer(client, client.world.backpack()).finished)
-cc = CutCloth(client)
+client.until(Delayed(client, 1).finished)
+cc = CutAllCloth(client)
 client.until(cc.finished)
