6d432c19a2c9afabe061fef308f765463cb43ec3
Max Kellermann <max@duempel.org>
2010-03-24 07:23:26 +0100

    added message dumper script

---
+++ src/messages.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+#  GemUO
+#
+#  (c) 2005-2010 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+
+from gemuo.simple import simple_run
+from gemuo.engine.messages import PrintMessages
+from gemuo.engine.guards import Guards
+
+def run(client):
+    Guards(client)
+    PrintMessages(client)
+
+simple_run(run)
