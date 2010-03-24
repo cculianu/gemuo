17d4d2725fcbb66db1d0d4e31d714bdd06817ddb
Max Kellermann <max@duempel.org>
2010-03-24 06:35:26 +0100

    added Carpentry training script

---
+++ src/carpentry.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+#
+#  GemUO
+#
+#  (c) 2005-2010 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+
+from uo.entity import *
+import uo.packets as p
+from gemuo.simple import SimpleClient
+from gemuo.data import TileCache
+from gemuo.entity import Item
+from gemuo.engine import Engine
+from gemuo.engine.messages import PrintMessages
+from gemuo.engine.guards import Guards
+from gemuo.engine.watch import Watch
+from gemuo.engine.walk import PathFindWalk
+from gemuo.engine.util import FinishCallback, DelayedCallback
+from gemuo.engine.restock import Restock, Trash
+from gemuo.engine.carpentry import Carpentry
+
+def find_box_at(world, x, y):
+    for e in world.iter_entities_at(x, y):
+        if isinstance(e, Item) and e.item_id in ITEMS_LARGE_CRATE:
+            return e
+    return None
+
+def find_restock_box(world):
+    """Find the large crate one tile north of the player.  It is used
+    for restocking."""
+    return find_box_at(world, world.player.position.x, world.player.position.y - 1)
+
+class HouseRestock(Engine):
+    def __init__(self, client):
+        Engine.__init__(self, client)
+
+        world = client.world
+        player = world.player
+        self._box = find_restock_box(world)
+        if self._box is None:
+            print "No box"
+            self._failure()
+            return
+
+        #FinishCallback(client, OpenContainer(client, box), self._box_opened)
+        #return
+
+        self._box_opened(True)
+
+    def _box_opened(self, success):
+        if not success:
+            self._failure()
+            return
+
+        counts = (
+            (ITEMS_CARPENTRY_TOOLS, 2),
+            (ITEMS_LOGS + ITEMS_BOARDS, 50),
+        )
+
+        client = self._client
+        FinishCallback(client, Restock(client, self._box, counts=counts),
+                       self._restocked)
+
+    def _restocked(self, success):
+        if not success:
+            self._failure()
+            return
+
+        self._success()
+
+class AutoCarpentry(Engine):
+    def __init__(self, client):
+        Engine.__init__(self, client)
+        self._trash()
+
+    def _trash(self):
+        client = self._client
+
+        FinishCallback(client, Trash(client, ITEMS_CARPENTRY_PRODUCTS), self._trashed)
+
+    def _trashed(self, success):
+        if not success:
+            self._failure()
+            return
+
+        client = self._client
+        FinishCallback(client, HouseRestock(client), self._restocked)
+
+    def _restocked(self, success):
+        if not success:
+            self._failure()
+            return
+
+        DelayedCallback(self._client, 1, self._restocked2)
+
+    def _restocked2(self):
+        FinishCallback(client, Carpentry(self._client), self._crafted)
+
+    def _crafted(self, success):
+        if not success:
+            self._failure()
+            return
+
+        DelayedCallback(self._client, 9, self._trash)
+
+client = SimpleClient()
+
+PrintMessages(client)
+Guards(client)
+Watch(client)
+client.until(AutoCarpentry(client).finished)
---
+++ src/gemuo/engine/carpentry.py
@@ -0,0 +1,70 @@
+#
+#  GemUO
+#
+#  (c) 2005-2010 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+
+from uo.skills import SKILL_CARPENTRY
+from uo.entity import *
+import uo.packets as p
+from gemuo.engine import Engine
+from gemuo.engine.util import FinishCallback
+from gemuo.engine.menu import MenuResponse
+
+def carpentry_target(skill):
+    if skill < 400:
+        return ('Containers', 'wooden box')
+    if skill < 480:
+        return ('Containers', 'medium crate')
+    if skill < 550:
+        return ('Misc. Add-Ons', 'ballot box')
+    if skill < 684:
+        return ('Weapons and Armor', 'wooden shield')
+    if skill < 740:
+        return ('Weapons and Armor', 'fishing pole')
+    if skill < 800:
+        return ('Weapons and Armor', 'quarter staff')
+    return ('Weapons and Armor', 'gnarled staff')
+
+class Carpentry(Engine):
+    def __init__(self, client):
+        Engine.__init__(self, client)
+
+        tool = client.world.find_item_in(client.world.backpack(), lambda x: x.item_id in ITEMS_CARPENTRY_TOOLS)
+        if tool is None:
+            print "No tool"
+            self._failure()
+            return
+
+        skills = client.world.player.skills
+        if skills is None or SKILL_CARPENTRY not in skills:
+            print "No carpentry skill"
+            self._failure()
+            return
+
+        target = carpentry_target(skills[SKILL_CARPENTRY].value)
+        if target is None:
+            print "No carpentry target"
+            self._failure()
+            return
+
+        client.send(p.Use(tool.serial))
+
+        FinishCallback(client, MenuResponse(client, target),
+                       self._responded)
+
+    def _responded(self, success):
+        if success:
+            self._success()
+        else:
+            #self._failure()
+            self._success()
