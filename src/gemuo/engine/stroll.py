b030ad516df68a83fc23081f03f8b7424e566d86
Max Kellermann <max@duempel.org>
2010-03-26 19:09:31 +0100

    StrollWestBritain: use DelayedCallback

--- src/gemuo/engine/stroll.py
+++ src/gemuo/engine/stroll.py
@@ -16,13 +16,12 @@
 import random
 from gemuo.path import Position
 from gemuo.engine import Engine
-from gemuo.timer import TimerEvent
 from gemuo.engine.walk import PathFindWalk
+from gemuo.engine.util import DelayedCallback
 
-class StrollWestBritain(Engine, TimerEvent):
+class StrollWestBritain(Engine):
     def __init__(self, client, map):
         Engine.__init__(self, client)
-        TimerEvent.__init__(self, client)
 
         self.map = map
         self.min_x, self.min_y = 1429,1540
@@ -32,21 +31,19 @@ class StrollWestBritain(Engine, TimerEvent):
         self._next_walk()
 
     def _next_walk(self):
+        client = self._client
         to = Position(self.random.randint(self.min_x, self.max_x),
                       self.random.randint(self.min_y, self.max_y))
-        self._walk = PathFindWalk(self._client, self.map, to)
+        self._walk = PathFindWalk(client, self.map, to)
         if self._walk.finished():
-            self._schedule(2)
-
-    def tick(self):
-        self._next_walk()
+            DelayedCallback(client, 2, self._next_walk)
 
     def on_engine_success(self, engine, *args, **keywords):
         if engine == self._walk:
             self._walk = None
-            self._schedule(2)
+            DelayedCallback(client, 2, self._next_walk)
 
     def on_engine_failure(self, engine, *args, **keywords):
         if engine == self._walk:
             self._walk = None
-            self._schedule(2)
+            DelayedCallback(client, 2, self._next_walk)
