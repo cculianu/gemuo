91da2083c69ff14b5ee6103a6afc64a8d953e352
Max Kellermann <max@duempel.org>
2010-03-28 21:01:46 +0200

    spin.py, weave.py: use engine UseAndTarget

--- src/spin.py
+++ src/spin.py
@@ -5,13 +5,11 @@ import uo.packets as p
 from uo.skills import *
 from uo.entity import *
 from gemuo.simple import simple_run, simple_later
-from gemuo.target import Target, SendTarget
-from gemuo.entity import Item
 from gemuo.engine import Engine
 from gemuo.engine.messages import PrintMessages
 from gemuo.engine.watch import Watch
 from gemuo.engine.items import OpenContainer, UseAndTarget
-from gemuo.engine.util import FinishCallback, Delayed, DelayedCallback
+from gemuo.engine.util import DelayedCallback
 from gemuo.engine.hide import AutoHide
 from gemuo.engine.restock import Restock
 from gemuo.engine.defer import defer_engine
@@ -37,32 +35,16 @@ class SpinWool(Engine):
 
     def _next(self):
         client = self._client
-        self.wool = find_wool(client.world)
-        if self.wool is None:
+        wool = find_wool(client.world)
+        if wool is None:
             self._success()
             return
 
-        self.target_mutex.get_target(self.target_ok, self.target_abort)
+        d = UseAndTarget(client, wool, self.spinningwheel).deferred
+        d.addCallbacks(self._done, self._failure)
 
-    def target_ok(self):
-        client = self._client
-        client.send(p.Use(self.wool.serial))
-
-        self.engine = SendTarget(client, Target(serial=self.spinningwheel.serial))
-        FinishCallback(client, self.engine, self._target_sent)
-
-    def target_abort(self):
-        self.engine.abort()
-        self._failure()
-
-    def _target_sent(self, success):
-        self.target_mutex.put_target()
-
-        if success:
-            DelayedCallback(self._client, 3.5, self._next)
-        else:
-            self._failure()
-
+    def _done(self, result):
+        DelayedCallback(self._client, 3.5, self._next)
 
 def backpack_opened(client):
     d = defer_engine(client, SpinWool(client))
--- src/weave.py
+++ src/weave.py
@@ -5,13 +5,11 @@ import uo.packets as p
 from uo.skills import *
 from uo.entity import *
 from gemuo.simple import simple_run, simple_later
-from gemuo.target import Target, SendTarget
-from gemuo.entity import Item
 from gemuo.engine import Engine
 from gemuo.engine.messages import PrintMessages
 from gemuo.engine.watch import Watch
 from gemuo.engine.items import OpenContainer, UseAndTarget
-from gemuo.engine.util import FinishCallback, Delayed, DelayedCallback
+from gemuo.engine.util import DelayedCallback
 from gemuo.engine.hide import AutoHide
 from gemuo.engine.restock import Restock
 from gemuo.engine.defer import defer_engine
@@ -36,32 +34,16 @@ class WeaveYarn(Engine):
 
     def _next(self):
         client = self._client
-        self.yarn = find_yarn(client.world)
-        if self.yarn is None or self.yarn.amount < 5:
+        yarn = find_yarn(client.world)
+        if yarn is None or yarn.amount < 5:
             self._success()
             return
 
-        self.target_mutex.get_target(self.target_ok, self.target_abort)
+        d = UseAndTarget(client, yarn, self.loom).deferred
+        d.addCallbacks(self._done, self._failure)
 
-    def target_ok(self):
-        client = self._client
-        client.send(p.Use(self.yarn.serial))
-
-        self.engine = SendTarget(client, Target(serial=self.loom.serial))
-        FinishCallback(client, self.engine, self._target_sent)
-
-    def target_abort(self):
-        self.engine.abort()
-        self._failure()
-
-    def _target_sent(self, success):
-        self.target_mutex.put_target()
-
-        if success:
-            DelayedCallback(self._client, 3.5, self._next)
-        else:
-            self._failure()
-
+    def _done(self, result):
+        DelayedCallback(self._client, 3.5, self._next)
 
 def backpack_opened(client):
     d = defer_engine(client, WeaveYarn(client))
