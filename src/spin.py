797e82d165b7c2088984ac54a964de1e4006077e
Max Kellermann <max@duempel.org>
2010-04-06 07:36:25 +0200

    spin.py, weave.py: use deferred_find_item_in_backpack()

--- src/spin.py
+++ src/spin.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python
 
-from twisted.internet import defer
-import uo.packets as p
-from uo.skills import *
 from uo.entity import *
-from gemuo.simple import simple_run, simple_later
+from gemuo.simple import simple_run
+from gemuo.defer import deferred_find_item_in_backpack
 from gemuo.engine import Engine
 from gemuo.engine.messages import PrintMessages
-from gemuo.engine.watch import Watch
-from gemuo.engine.items import OpenContainer, UseAndTarget
+from gemuo.engine.items import UseAndTarget
 from gemuo.engine.util import DelayedCallback
 from gemuo.engine.hide import AutoHide
-from gemuo.engine.restock import Restock
-from gemuo.engine.defer import defer_engine
-
-def find_wool(world):
-    return world.find_player_item(lambda x: x.item_id == ITEM_WOOL)
 
 def find_reachable_spinning_wheel(world):
     return world.find_reachable_item(lambda x: x.item_id in ITEMS_SPINNING_WHEEL)
@@ -24,7 +16,6 @@ class SpinWool(Engine):
     def __init__(self, client):
         Engine.__init__(self, client)
 
-        self.target_mutex = client.target_mutex
         self.spinningwheel = find_reachable_spinning_wheel(client.world)
         print self.spinningwheel
         if self.spinningwheel is None:
@@ -34,35 +25,20 @@ class SpinWool(Engine):
         self._next()
 
     def _next(self):
-        client = self._client
-        wool = find_wool(client.world)
-        if wool is None:
-            self._success()
-            return
+        d = deferred_find_item_in_backpack(self._client, lambda x: x.item_id == ITEM_WOOL)
+        d.addCallbacks(self._found_wool, self._success)
 
-        d = UseAndTarget(client, wool, self.spinningwheel).deferred
+    def _found_wool(self, wool):
+        d = UseAndTarget(self._client, wool, self.spinningwheel).deferred
         d.addCallbacks(self._done, self._failure)
 
     def _done(self, result):
         DelayedCallback(self._client, 3.5, self._next)
 
-def backpack_opened(client):
-    d = defer_engine(client, SpinWool(client))
-    return d
-
-def backpack_opened0(result, *args, **keywords):
-    return simple_later(1, backpack_opened, *args, **keywords)
-
 def run(client):
     PrintMessages(client)
     AutoHide(client)
 
-    backpack = client.world.backpack()
-    if backpack is None:
-        return defer.fail('No backpack')
-
-    d = defer_engine(client, OpenContainer(client, backpack))
-    d.addCallback(backpack_opened0, client)
-    return d
+    return SpinWool(client)
 
 simple_run(run)
--- src/weave.py
+++ src/weave.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python
 
-from twisted.internet import defer
-import uo.packets as p
-from uo.skills import *
 from uo.entity import *
-from gemuo.simple import simple_run, simple_later
+from gemuo.simple import simple_run
+from gemuo.defer import deferred_find_item_in_backpack
 from gemuo.engine import Engine
 from gemuo.engine.messages import PrintMessages
-from gemuo.engine.watch import Watch
-from gemuo.engine.items import OpenContainer, UseAndTarget
+from gemuo.engine.items import UseAndTarget
 from gemuo.engine.util import DelayedCallback
 from gemuo.engine.hide import AutoHide
-from gemuo.engine.restock import Restock
-from gemuo.engine.defer import defer_engine
-
-def find_yarn(world):
-    return world.find_player_item(lambda x: x.item_id in ITEMS_YARN)
 
 def find_reachable_loom(world):
     return world.find_reachable_item(lambda x: x.item_id in ITEMS_LOOM)
@@ -33,35 +25,20 @@ class WeaveYarn(Engine):
         self._next()
 
     def _next(self):
-        client = self._client
-        yarn = find_yarn(client.world)
-        if yarn is None or yarn.amount < 5:
-            self._success()
-            return
+        d = deferred_find_item_in_backpack(self._client, lambda x: x.item_id in ITEMS_YARN and x.amount >= 5)
+        d.addCallbacks(self._found_yarn, self._success)
 
-        d = UseAndTarget(client, yarn, self.loom).deferred
+    def _found_yarn(self, yarn):
+        d = UseAndTarget(self._client, yarn, self.loom).deferred
         d.addCallbacks(self._done, self._failure)
 
     def _done(self, result):
         DelayedCallback(self._client, 3.5, self._next)
 
-def backpack_opened(client):
-    d = defer_engine(client, WeaveYarn(client))
-    return d
-
-def backpack_opened0(result, *args, **keywords):
-    return simple_later(1, backpack_opened, *args, **keywords)
-
 def run(client):
     PrintMessages(client)
     AutoHide(client)
 
-    backpack = client.world.backpack()
-    if backpack is None:
-        return defer.fail('No backpack')
-
-    d = defer_engine(client, OpenContainer(client, backpack))
-    d.addCallback(backpack_opened0, client)
-    return d
+    return WeaveYarn(client)
 
 simple_run(run)
