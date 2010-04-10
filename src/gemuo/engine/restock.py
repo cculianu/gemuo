fbfda7ced064a5451dddbcf0b21936cee257faad
Max Kellermann <max@duempel.org>
2010-04-10 22:41:21 +0200

    engine.restock: relax stack check

--- src/gemuo/engine/restock.py
+++ src/gemuo/engine/restock.py
@@ -22,7 +22,7 @@ from gemuo.engine.items import OpenContainer
 
 def drop_into(client, item, container, amount=0xffff):
     dest = client.world.find_item_in(container, lambda x: x.item_id == item.item_id and x.hue == item.hue and x.amount < 60000)
-    if dest is None or dest.amount == 1:
+    if dest is None or (dest.amount == 1 and item.amount == 1):
         dest = container
     client.send(p.LiftRequest(item.serial, amount))
     client.send(p.Drop(item.serial, 0, 0, 0, dest.serial))
